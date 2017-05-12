ded87f76d4619226ee4efaf02cf79023b90614c0
tahuana <tahuana@gmail.com>
2017-05-12 22:15:14 +0000

    Adds loop-problems

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+var count = 0;
+while (count < ingredients.length) {
+  console.log(ingredients[count]);
+  count += 1;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (var i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (var i = (ingredients.length - 1); i >= 0; i--){
+  console.log(ingredients[i]);
+}
